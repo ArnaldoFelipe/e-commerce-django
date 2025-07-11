478f2613def26b61fb843d5828d8cbc287cd9b58
arnaldo felipe <feelipesilvaa11@gmail.com>
2025-07-11 19:40:38 -0300

    iniciando os commits do projeto, o projeto ja esta sendo desenvolvido com os models e perfis ja criados

---
+++ produto/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from . import models
+
+class VariacaoInLine(admin.TabularInline):
+    model = models.Variacao
+    extra = 1
+    
+class ProdutoAdmin(admin.ModelAdmin):
+    inlines = [
+        VariacaoInLine
+    ]
+
+admin.site.register(models.Produto)
+admin.site.register(models.Variacao)
